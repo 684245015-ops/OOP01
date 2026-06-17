04dcdaad8cb59438f21656795082df4995315b0c
684245015-ops <684245015@webmail.npru.ac.th>
2026-06-17 17:10:15 +0700

    ส่ง Lab1_4

--- Lab1_4.ts
+++ Lab1_4.ts
@@ -1,10 +1,10 @@
 let Groseincome: number = 50000;
-let Taxrate: number =  7;
-let Taxpaid : number = 3500;
+let Taxpay: number = Groseincome*0.07;
+let aftertax: number = Groseincome-Taxpay;
 
 console.log ("----- Tax Calculation Report-----");
-console.log ("Groseincome %d",Groseincome);
-console.log ("Taxrate %d",Taxrate);
-console.log ("Taxpain %d", Taxpaid);
-console.log ("Net Income after Tax : $%d",Groseincome-Taxpaid);
+console.log ("Groseincome income %d",Groseincome);
+console.log ("Taxrate : 7%");
+console.log ("Taxpain : %d", Taxpay);
+console.log ("Net Income after Tax : $%d",aftertax);
 console.log ("---------------------------------");
