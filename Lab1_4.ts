2790ff35f970b499f2ba6eeefcb1d27047d0875b
684245015-ops <684245015@webmail.npru.ac.th>
2026-06-17 16:50:45 +0700

    Lab1_4

---
+++ Lab1_4.ts
@@ -0,0 +1,10 @@
+let Groseincome: number = 50000;
+let Taxrate: number =  7;
+let Taxpaid : number = 3500;
+
+console.log ("----- Tax Calculation Report-----");
+console.log ("Groseincome %d",Groseincome);
+console.log ("Taxrate %d",Taxrate);
+console.log ("Taxpain %d", Taxpaid);
+console.log ("Net Income after Tax : $%d",Groseincome-Taxpaid);
+console.log ("---------------------------------");
